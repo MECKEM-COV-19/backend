612209ecd1b589fa4b6f275774d0e73ad54e909a
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2020-03-22 12:50:12 +0100

    Reorganized project

--- django_backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/philipp/Schreibtisch/wirvsvirus/git/backend/django_backend/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
